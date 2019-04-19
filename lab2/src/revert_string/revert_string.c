1fd85d2fb39c3918bc05ee2ce61d0c9ba238cdca
Ilya <elbruselle@gmail.com>
2019-04-19 08:15:30 +0000

    lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,13 @@
+
 #include "revert_string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+	for (int i=0; i< strlen(str)/2;i++)
+	{
+	    char buff =str[i];
+	    str[i]=str[strlen(str)-i-1];
+	    str[strlen(str)-i-1]=buff;
+	    
+	}
 }
-
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -1,6 +1,9 @@
+    
 #include "swap.h"
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+    *left ^= *right;
+    *right ^= *left;
+    *left ^= *right;
 }
